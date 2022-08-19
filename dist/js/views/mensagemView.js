c51fbc58a61056601c8ff49c00d78e9c28500a24
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2022-08-19 14:07:56 -0300

    Reestruturando projeto

--- dist/js/views/mensagemView.js
+++
@@ -1,9 +0,0 @@
-import View from "./view.js";
-class MensagemView extends View {
-    template(model) {
-        return `
-            <p class = "alert alert-info">${model}</p>
-        `;
-    }
-}
-export default MensagemView;
